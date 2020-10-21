dec107b65a431e836f325b190277ce68bff55029
capWailing <thomasyantom@gmail.com>
2020-10-21 17:29:24 -0400

    Initial commit

---
+++ src/main/java/com/seekers/seekerback/SeekerBackApplication.java
@@ -0,0 +1,13 @@
+package com.seekers.seekerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeekerBackApplication.class, args);
+	}
+
+}
