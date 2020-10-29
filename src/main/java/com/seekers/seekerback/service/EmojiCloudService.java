93e1652fa43d63e81ef96076fa7a124ba7e17bea
FH18 <48007526+wwwqllll@users.noreply.github.com>
2020-10-30 07:29:24 +0800

    can run now

--- src/main/java/com/seekers/seekerback/service/EmojiCloudService.java
+++ src/main/java/com/seekers/seekerback/service/EmojiCloudService.java
@@ -9,7 +9,6 @@ import com.kennycason.kumo.bg.PixelBoundryBackground;
 import com.kennycason.kumo.font.KumoFont;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
 import com.kennycason.kumo.nlp.FrequencyAnalyzer;
-
 import com.kennycason.kumo.palette.ColorPalette;
 import com.kennycason.kumo.palette.LinearGradientColorPalette;
 
@@ -19,63 +18,124 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import com.alibaba.fastjson.*;
+import com.seekers.seekerback.util.database.ISearchService;
+import com.seekers.seekerback.util.database.impl.SearchServiceImpl;
 
 public class EmojiCloudService {
 
-    public static String getEmojiCloud() throws IOException {
+    public static List<WordFrequency> getEmojiFrequency (int id) {
 
 
-        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        frequencyAnalyzer.setWordFrequenciesToReturn(300);
-        frequencyAnalyzer.setMinWordLength(4);
-        //frequencyAnalyzer.setStopWords(loadStopWords());
+//        List<String> text = new ArrayList();
+//        text.add("\uD83D\uDE32sdfsadf");
+//        text.add("\uD83D\uDC44fsdf");
+//        text.add("kumokumo");
 
-        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("C:/Users/wengq/OneDrive - stevens.edu/695/project/be695/SSW695_Seekers/src/main/java/com/seekers/seekerback/text/emoji.txt");
-        final Dimension dimension = new Dimension(500, 312);
-        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+//        ISearchService iSearchService = new SearchServiceImpl("localhost", 9200);
+//        for (int i = 0; i < 10; i++){
+//            iSearchService.getByJson(id);
+//
+//
+//        }
 
-        wordCloud.setKumoFont(new KumoFont(new File("C:/Users/wengq/OneDrive - stevens.edu/695/project/be695/SSW695_Seekers/src/main/java/com/seekers/seekerback/font/OpenSansEmoji.ttf")));
-        //wordCloud.setKumoFont(new KumoFont(EmojiCloud.class.getResourceAsStream("C:/Users/wengq/OneDrive - stevens.edu/695/project/be695/SSW695_Seekers/src/main/java/com/seekers/seekerback/font/symbol.ttf")));
-        wordCloud.setPadding(2);
-        //wordCloud.setBackground(new PixelBoundryBackground("backgrounds/whale_small.png"));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
-        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
-        wordCloud.build(buildWordFrequencies());
-        wordCloud.writeToFile("emoji_cloud_small.png");
 
-        return "url_emoji_cloud";
-    }
 
-    private static final Random RANDOM = new Random();
+//get data from database as json, parse it into a emoji word frequency
+//not finished, for testing
 
-    private static List<WordFrequency> buildWordFrequencies() {
         final List<WordFrequency> wordFrequencies = new ArrayList<>();
         for (final String emoji : EMOJIS) {
             wordFrequencies.add(new WordFrequency(emoji, RANDOM.nextInt(250)));
         }
         return wordFrequencies;
+
     }
 
+
+    public static String getEmojiCloudGraph(int id) throws IOException {
+
+        //analyzer settings
+
+        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        frequencyAnalyzer.setWordFrequenciesToReturn(300);
+        //frequencyAnalyzer.setMinWordLength(4);
+        //frequencyAnalyzer.setStopWords(loadStopWords());
+
+        //load frequency using getEmojiFrequency, need to give id to it
+
+        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.loadWordFrequencies(getEmojiFrequency(id));
+
+        //new wordcloud object
+
+        final Dimension dimension = new Dimension(250, 250);
+        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+
+        //set right font for emoji display ****IMPORTANT*****
+        wordCloud.setKumoFont(new KumoFont(new File("font/OpenSansEmoji.ttf")));
+        //wordCloud.setKumoFont(new KumoFont(EmojiCloud.class.getResourceAsStream("font/OpenSansEmoji.ttf")));
+
+        //format settings. can be modified later.
+
+        wordCloud.setPadding(2);
+        //wordCloud.setBackground(new PixelBoundryBackground("backgrounds/whale_small.png"));
+        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+
+        //build wordcloud with wordFrequencies at line 65
+        wordCloud.build(wordFrequencies);
+        wordCloud.writeToFile("emoji_cloud_small.png");
+
+        return "url_emoji_cloud";
+    }
+
+
+
+    //for testing
+    private static final Random RANDOM = new Random();
+
     private static final String[] EMOJIS = {
-            "emoji",
-            "kumo",
-            "\uD83D\uDE02",
+            "TEST6858678",
             "\uD83D\uDC4D",
             "\uD83D\uDE0A",
             "\uD83D\uDE01",
             "\uD83D\uDE12",
             "\uD83D\uDE11",
+            "\uD83D\uDE0D",
+            "\uD83D\uDC4C",
+            "\uD83D\uDC55",
+            "\uD83D\uDCAA",
+            "\uD83D\uDE4C",
+            "\uD83D\uDE2D",
+            "\uD83D\uDE29",
+            "\uD83D\uDE21",
             "\uD83D\uDD25",
             "\uD83D\uDE33",
+            "\uD83D\uDD25",
+            "\uD83D\uDE21",
+            "\uD83D\uDCA9",
+            "\uD83D\uDC7B",
+            "\uD83D\uDC7D",
+            "\uD83D\uDC57",
+            "\uD83D\uDD25",
+            "\uD83C\uDF53",
+            "\uD83C\uDFB1",
+            "\uD83C\uDFA7",
+            "\uD83D\uDE91",
+            "\uD83C\uDFEB",
+            "\uD83D\uDCBE",
             "\uD83D\uDCAF",
             "\uD83D\uDE32",
             "\uD83D\uDE31",
             "\uD83D\uDE0F",
             "\uD83D\uDC44",
-            "\uD83D\uDC40 "
+            "\uD83D\uDC40"
     };
 
+
     public static void main(String[] args) throws IOException{
-        getEmojiCloud();
+        getEmojiCloudGraph(0);
     }
+
+
 }
--- src/test/java/com/seekers/seekerback/ISearchServiceTest.java
+++ src/test/java/com/seekers/seekerback/ISearchServiceTest.java
@@ -22,11 +22,12 @@ public class ISearchServiceTest {
 //        for (String e: result) {
 //            System.out.println(iSearchService.get("twitter", e));
 //        }
+
         List<String> idList = new ArrayList<>();
         idList.add("1320508566822739969");
         idList.add("1321567946133569536");
         List<Map<String, Object>> mapList = new ArrayList<>();
-        mapList = (List<Map<String, Object>>) JSON.parse("[{\"created_at\":\"2020-10-25T23:32:30.000Z\",\"text\":\"201026 #TEN Instagram Update \\n\\n#WayV #WeiShenV #威神V #李永钦 https://t.co/uLcmnCOgn8\"},{\"created_at\":\"2020-10-28T21:42:06.000Z\",\"text\":\"(T/N: this is the MV for \\\"More\\\" by K/DA)\"}]");
+        mapList = (List<Map<String, Object>>) JSON.parse("[{\"created_at\":\"2020-10-25T23:33:45.000Z\",\"text\":\"@LionessMo\\uD83D\\uDCAFnbebe \\uD83D\\uDCAF@WayV_official \\uD83D\\uDE0FHehe thank you!! \\uD83D\\uDE0FI just love it so much \\uD83D\\uDE04\\uD83D\\uDE04\\uD83D\\uDE04\\uD83D\\uDE04\"},{\"created_at\":\"2020-10-28T21:42:06.000Z\",\"text\":\"(T/N\\uD83D\\uDE0F\\uD83D\\uDE0F\\uD83D\\uDE0F: this is the MV \\uD83D\\uDD25\\uD83D\\uDD25for \\\"More\\\" by \\uD83D\\uDE33\\uD83D\\uDE33K/DA)\"}]");
         System.out.println(iSearchService.bulkUpsert("twitter", idList, mapList));
 
         iSearchService.close();
