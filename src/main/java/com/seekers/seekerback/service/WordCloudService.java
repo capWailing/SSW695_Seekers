e2543f5dc278956bfc1c25c60c7208947971f14b
kzh123 <34299029+kzh123@users.noreply.github.com>
2020-11-05 10:39:47 +0800

    word cloud service complete

--- src/main/java/com/seekers/seekerback/service/WordCloudService.java
+++ src/main/java/com/seekers/seekerback/service/WordCloudService.java
@@ -13,6 +13,7 @@ import com.seekers.seekerback.util.database.impl.SearchServiceImpl;
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -61,14 +62,21 @@ public class WordCloudService {
 //        text3.add(text2);
 
         //Kumo API start from here, include frequency analyzer and image generate.
-        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        List<String> stopword = new ArrayList<>();
+        stopword.add("the");
+
+        FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        frequencyAnalyzer.setStopWords(stopword);
+
         final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load(text3);
         final Dimension dimension = new Dimension(600, 600);
+
         com.kennycason.kumo.WordCloud wordCloud = new com.kennycason.kumo.WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+
         wordCloud.setPadding(2);
         wordCloud.setBackground(new CircleBackground(300));
         wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
-        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+        wordCloud.setFontScalar(new SqrtFontScalar(10,100));
         wordCloud.build(wordFrequencies);
         //TODO: change the filePath
         String filePath = "./datarank_wordcloud_circle_sqrt_font.png";
