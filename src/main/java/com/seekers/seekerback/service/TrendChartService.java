d0b01b360767c635bd810bef1ed2e775c7d624dd
FH18 <48007526+wwwqllll@users.noreply.github.com>
2020-11-15 23:30:45 +0800

    trend chart done2

--- src/main/java/com/seekers/seekerback/service/TrendChartService.java
+++ src/main/java/com/seekers/seekerback/service/TrendChartService.java
@@ -2,6 +2,7 @@ package com.seekers.seekerback.service;
 
 
 import java.awt.Font;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -86,7 +87,7 @@ public class TrendChartService {
      * 创建JFreeChart对象
      * @return chart
      */
-    public static JFreeChart createChart() {
+    public static ByteArrayOutputStream createChart() throws IOException {
         StandardChartTheme standardChartTheme = new StandardChartTheme("CN"); //创建主题样式
         standardChartTheme.setExtraLargeFont(new Font("隶书", Font.BOLD, 20)); //设置标题字体
         standardChartTheme.setRegularFont(new Font("宋体", Font.PLAIN, 15)); //设置图例的字体
@@ -102,28 +103,13 @@ public class TrendChartService {
                 true,//是否显示图例标识
                 false,//是否显示tooltips
                 false);//是否支持超链接
-        return chart;
+
+
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ChartUtilities.writeChartAsJPEG(outputStream, chart, 600, 400);
+
+        return outputStream;
     }
-    // 本地测试
-    public static void main(String[] args) throws IOException {
 
-
-
-
-//        createDataSet();
-
-//        ChartFrame cf = new ChartFrame("Test", createChart());
-//        cf.pack();
-//        cf.setVisible(true);
-
-
-
-        JFreeChart chart= createChart();
-
-        OutputStream os = new FileOutputStream("company.jpeg");//图片是文件格式的，故要用到FileOutputStream用来输出。
-        ChartUtilities.writeChartAsJPEG(os, chart, 1000, 800);
-        //使用一个面向application的工具类，将chart转换成JPEG格式的图片。第3个参数是宽度，第4个参数是高度。
-
-        os.close();//关闭输出流
-    }
 }
