c84ab6eba5b7ff1670d87f1ae07567285c6d1bc5
FH18 <48007526+wwwqllll@users.noreply.github.com>
2020-11-15 23:15:05 +0800

    trend chart done

--- src/main/java/com/seekers/seekerback/service/TrendChartService.java
+++ src/main/java/com/seekers/seekerback/service/TrendChartService.java
@@ -5,7 +5,14 @@ import java.awt.Font;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.kennycason.kumo.WordFrequency;
+import com.seekers.seekerback.util.database.ISearchService;
+import com.seekers.seekerback.util.database.impl.SearchServiceImpl;
 import org.jfree.chart.*;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
@@ -15,12 +22,64 @@ import org.jfree.data.category.DefaultCategoryDataset;
 public class TrendChartService {
 
     public static CategoryDataset createDataSet() {
+
+
+
+        List<Map<String, Object>> text1_json = new ArrayList<>();
+
+        List<String> text3 = new ArrayList<>();
+
+        ISearchService iSearchService = new SearchServiceImpl("localhost", 9200);
+        List<String> result = iSearchService.idQuery("twitter");
+        for (String e : result) {
+//            System.out.println(e);
+//            System.out.println(iSearchService.get("twitter", e));
+            text1_json.add(iSearchService.get("twitter", e));
+        }
+
+
+        iSearchService.close();
+        //System.out.println(text1_json);
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        String date = "";
+        int temp = 0;
+
+        for (Map<String, Object> e : text1_json) {
+            date = e.get("created_at").toString().substring(0,10);
+            //System.out.println(e.get("created_at").toString().substring(0,10));
+
+            if (map.get(date) == null) {
+                map.put(date, 1);
+            } else {
+                temp = map.get(date)+1;
+                map.put(date, temp);
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
         // 实例化DefaultCategoryDataset对象
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
         // 向数据集合中添加数据
-        dataSet.addValue(500, "Java图书", "J2SE类");
-        dataSet.addValue(100, "Java图书", "J2ME类");
-        dataSet.addValue(900, "Java图书", "J2EE类");
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+
+            String key = entry.getKey();
+
+            Integer value = entry.getValue();
+            dataSet.addValue(value, "tweet", key);
+
+
+        }
+
         return dataSet;
     }
     /**
@@ -35,9 +94,9 @@ public class TrendChartService {
         ChartFactory.setChartTheme(standardChartTheme);//设置主题样式
         // 通过ChartFactory创建JFreeChart
         JFreeChart chart = ChartFactory.createBarChart3D(
-                "Java图书销量统计", //图表标题
-                "Java图书", //横轴标题
-                "销量（本）",//纵轴标题
+                "Tweet Trend Chart", //图表标题
+                "Date", //横轴标题
+                "tweet count",//纵轴标题
                 createDataSet(),//数据集合
                 PlotOrientation.VERTICAL, //图表方向
                 true,//是否显示图例标识
@@ -47,9 +106,18 @@ public class TrendChartService {
     }
     // 本地测试
     public static void main(String[] args) throws IOException {
+
+
+
+
+//        createDataSet();
+
 //        ChartFrame cf = new ChartFrame("Test", createChart());
 //        cf.pack();
 //        cf.setVisible(true);
+
+
+
         JFreeChart chart= createChart();
 
         OutputStream os = new FileOutputStream("company.jpeg");//图片是文件格式的，故要用到FileOutputStream用来输出。
